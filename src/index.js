69d02e10f997706586b1f3e6c41d647b59f5c89e
ebrahemsaleh <salehebrahem487@gmail.com>
2020-09-29 22:18:14 +0200

    install styledcomponent , react-router, firebase ,fuse and normalize

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import App from './app';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootEl = document.getElementById('root');
+
+let render = () => {
+
+  ReactDom.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootEl
+  );
+
+}
+
+
+if (module.hot) {
+  module.hot.accept('./app.js', () => {
+    setTimeout(render)
+  }) 
+}
+
+
+render();
+
+
+
+
+
