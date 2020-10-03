2ec1b307be2f4bd5c205532b91acf7bea58ef589
ebrahemsaleh <salehebrahem487@gmail.com>
2020-10-03 02:35:29 +0200

    add jumb component and style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import  Jumbotron  from './components/jumbotron';
+import jumboDate from './fixtures/jumbo';
 
+export default function App  ()   {
+  return (  
+    <Jumbotron.Container>
 
+      {jumboDate.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction} >
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane> 
 
-function App() {
-  return (
-    <div className="App">
-    hello 
-    </div>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+          </Jumbotron>
+      ))}
+          
+    </Jumbotron.Container>
   );
 }
 
-export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import App from './app';
+import ReactDOM from 'react-dom';
+import  App  from './app';
 
 
-const rootEl = document.getElementById('root');
 
-let render = () => {
-
-  ReactDom.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootEl
-  );
-
-}
-
-
-if (module.hot) {
-  module.hot.accept('./app.js', () => {
-    setTimeout(render)
-  }) 
-}
-
-
-render();
-
-
+ReactDOM.render(<App/>,document.getElementById('root'));
 
 
 
