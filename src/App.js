546b90bf9a14120cd02d12512cb76ee708f009fa
ebrahemsaleh <salehebrahem487@gmail.com>
2020-10-10 20:42:23 +0200

    edit to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch , BrowserRouter as Router , Route} from 'react-router-dom';
+import {  BrowserRouter as Router , Route} from 'react-router-dom';
 import {Home , Browse ,SignIn ,SignUp} from './pages';
 import * as ROUTES from './constants/routes';
 
--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import { GlobalStyles } from './global-style';
 import  App  from './app';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
+
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
   ReactDOM.render(
     <>
+      <FirebaseContext.Provider value={{firebase}} >
       <GlobalStyles />
       <App />
+      </FirebaseContext.Provider>
     </>
     , rootEl);
 }
