3b93bea332ce7e6756b793f4960fa2267497ea8d
ebrahemsaleh <salehebrahem487@gmail.com>
2020-11-26 21:39:01 +0200

    edit to test player component

--- src/__tests__/components/player.test.js
+++ src/__tests__/components/player.test.js
@@ -3,8 +3,10 @@ import { render, fireEvent } from '@testing-library/react';
 import { Player } from '../../components';
 
 describe('<Player />', () => {
+
   it('renders the <Player /> with a bunny video', () => {
     const { container, getByText, queryByTestId } = render(
+
       <Player>
         <Player.Button />
         <Player.Video src="/videos/bunny.mp4" />
@@ -13,8 +15,10 @@ describe('<Player />', () => {
 
     expect(queryByTestId('player')).toBeFalsy();
     fireEvent.click(getByText('Play'));
+
     expect(queryByTestId('player')).toBeTruthy();
     fireEvent.click(queryByTestId('player'));
+    
     expect(queryByTestId('player')).toBeFalsy();
     expect(container.firstChild).toMatchSnapshot();
   });
