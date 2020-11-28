366df049c12202a06caa4cc1076f9ed0a3a49e38
ebrahemsaleh <salehebrahem487@gmail.com>
2020-11-28 13:31:22 +0200

    test and snapshot loading component

---
+++ src/__tests__/components/loading.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Loading } from '../../components';
+
+
+describe('<Loading/>', () => {
+  it('', () => {
+    const { container, getByTestId } = render(<Loading src="/images/karl.png" data-testid="loading" />)
+    
+    expect(getByTestId('loading')).toBeTruthy();
+    expect(getByTestId('loading-picture')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  })
+})
