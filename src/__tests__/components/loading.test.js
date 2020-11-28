d52885f9b45436fde17dd52b686a896e4c8b822d
ebrahemsaleh <salehebrahem487@gmail.com>
2020-11-28 13:41:29 +0200

    add thing in testing loading

--- src/__tests__/components/loading.test.js
+++ src/__tests__/components/loading.test.js
@@ -10,5 +10,10 @@ describe('<Loading/>', () => {
     expect(getByTestId('loading')).toBeTruthy();
     expect(getByTestId('loading-picture')).toBeTruthy();
     expect(container.firstChild).toMatchSnapshot();
-  })
+  });
+
+  it('renders the <Loading.ReleaseBody />', () => {
+    const { container } = render(<Loading.ReleaseBody />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
 })
