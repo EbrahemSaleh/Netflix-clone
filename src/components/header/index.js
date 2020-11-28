3593e8d54afbafcff8e1af5c189edc0f703930e5
ebrahemsaleh <salehebrahem487@gmail.com>
2020-11-28 15:36:54 +0200

    edit in header search component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -21,7 +21,7 @@ import {
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? (
-    <Background  {...restProps}>
+    <Background  {...restProps} data-testid="header-bg" >
       {children}
     </Background>
   ) : (
@@ -50,18 +50,20 @@ Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps 
 
   return (
     <Search {...restProps}>
-      <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} >
+      <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} data-testid="search-click">
         <img src="/images/icons/search.png" alt="Search" />
       </SearchIcon>
       <SearchInput
         value={searchTerm}
-        onChange={( {target} ) => setSearchTerm(target.value)}
+        onChange={({ target }) => setSearchTerm(target.value)}
         placeholder="Search films and series"
         active={searchActive}
+        data-testid="search-input"
       />
     </Search>
   );
 };
+
 
 Header.Profile = function HeaderProfile({ children, ...restProps }) {
   return <Profile {...restProps}>{children}</Profile>;
@@ -72,7 +74,7 @@ Header.Feature = function HeaderFeature({ children, ...restProps }) {
 };
 
 Header.Picture = function HeaderPicture({ src, ...restProps }) {
-  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+  return <Picture {...restProps} src={`/images/users/${src}.png`}  />;
 };
 
 Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
